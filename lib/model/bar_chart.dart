d893a3e313d45931ef494b2f03cb332a2a6cbce8
SBaro <santaigo.baro@fing.edu.uy>
2020-03-21 14:46:13 -0300

    deleated unused code

--- lib/model/bar_chart.dart
+++ lib/model/bar_chart.dart
@@ -8,13 +8,6 @@ class SimpleBarChart extends StatelessWidget {
   SimpleBarChart(this.seriesList, {this.animate});
 
   /// Creates a [BarChart] with sample data and no transition.
-  factory SimpleBarChart.withSampleData() {
-    return new SimpleBarChart(
-      _createSampleData(),
-      // Disable animations for image tests.
-      animate: false,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,35 +16,4 @@ class SimpleBarChart extends StatelessWidget {
       animate: false,
     );
   }
-
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<OrdinalSales, String>> _createSampleData() {
-    final data = [
-      new OrdinalSales('2014', 5),
-      new OrdinalSales('2015', 25),
-      new OrdinalSales('2016', 100),
-      new OrdinalSales('2017', 75),
-      new OrdinalSales('2018', 35),
-      new OrdinalSales('2019', 87),
-      new OrdinalSales('2020', 60),
-    ];
-
-    return [
-      new charts.Series<OrdinalSales, String>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (OrdinalSales sales, _) => sales.year,
-        measureFn: (OrdinalSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
-}
-
-/// Sample ordinal data type.
-class OrdinalSales {
-  final String year;
-  final int sales;
-
-  OrdinalSales(this.year, this.sales);
 }
